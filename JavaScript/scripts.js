b297410bcd7b2f81b2b313039c752abacdb15e93
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-01 20:52:58 -0500

    added javascript folder to practice

---
+++ JavaScript/scripts.js
@@ -0,0 +1,15 @@
+//
+let age = 20;
+
+if (age >= 21) {
+  alert("you can go to the club");
+} else if (age < 21) {
+  alert("oh no!");
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    if(i%2 === 0) {
+        console.log(i)
+    }
+}
