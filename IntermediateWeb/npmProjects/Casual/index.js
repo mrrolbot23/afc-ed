8a520b4dfee88c13eb6bc066c923fe82d47bd1f0
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-19 09:05:50 -0500

    added Casual project

---
+++ IntermediateWeb/npmProjects/Casual/index.js
@@ -0,0 +1,12 @@
+const casual = require("casual");
+
+console.log(`Hello there ${casual.name}!
+How was your trip to ${casual.country}?
+I sure hope you had a wonderful time.
+Is your phone number still ${casual.phone}?
+I will try to give you a call sometime. By the way, I want to send you a fresh loaf of bread at your address of:
+
+${casual.address}
+        
+Well, I will see you soon. I will be visiting sometime before ${casual.month_name}.
+Untile then, farewell!`);
