bf5d53359fad7fe45af47f916db2e5744a7524c7
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-19 10:44:02 -0500

    last hearts project update

--- IntermediateWeb/npmProjects/HeartsProject/index.js
+++ IntermediateWeb/npmProjects/HeartsProject/index.js
@@ -1,15 +1,20 @@
 const asciiHeart = require("ascii-heart");
 
-console.log("Default Heart");
+console.log("Default Heart.\n");
 console.log(asciiHeart());
 
-console.log("Small Heart.");
+console.log("Small Heart.\n");
 console.log(asciiHeart(10, 10));
 
-console.log("Large Heart");
+console.log("Large Heart.\n");
 console.log(asciiHeart(40, 40));
 
-console.log("Heart filled with JSON");
+console.log("Heart filled with JSON\n");
 console.log(asciiHeart( 30, 30, {
     fill: "json"
+}));
+
+console.log("Heart filled with aliens 👽.\n");
+console.log(asciiHeart( 30, 30, {
+    fill: "👽"
 }));
