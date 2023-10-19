439560ba5331f66a2c7e79b3adcd5011d36e5202
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-19 10:38:53 -0500

    Added readline-sync project

--- IntermediateWeb/npmProjects/RedlineProject/index.js
+++ IntermediateWeb/npmProjects/RedlineProject/index.js
@@ -9,7 +9,42 @@ let favoriteFood = readLineSync.question("\nWhat is your favorite food? ");
 
 let favoriteDrink = readLineSync.question("\nWhat is your favorite drink? ");
 
-// Response with answers 
+// Response with answers
 console.log(
   `\nHi ${name}, your favorite food is ${favoriteFood} and your favorite drink is ${favoriteDrink}.\n`
 );
+
+
+// Part 2
+console.log("-----------------------Part 2-------------------------");
+
+const spicyTacos = () => {
+    // Ask the user how spicy they want their tacos
+  (spicyChoice = [
+    "spicy",
+    "very spicy",
+    "so spicy, you won't be able to feel your face",
+  ]),
+    (index = readLineSync.keyInSelect(
+      spicyChoice,
+      "How spicy would you like your tacos? "
+    ));
+
+  if (spicyChoice[index] !== undefined) {
+    //   Response confirmation
+    let yesNo = readLineSync.question(
+      `\nOk, so you want your tacos to be ${spicyChoice[index]}. Are you sure about that?\nYes or No... `
+    );
+    // Response if the user does not cancel
+    if (yesNo.toLowerCase() === "yes") {
+      console.log("\nOk, we will have your order right out.");
+    } else {
+      console.log("\nWhat's the matter? Can't handle the heat?");
+    }
+  } else {
+    //   Response if the user cancels
+    console.log("\nOk, no tacos for you then!");
+  }
+};
+
+spicyTacos();
