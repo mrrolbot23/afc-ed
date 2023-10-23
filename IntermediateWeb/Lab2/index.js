2f195d49964a950c5bc79dc11278620c8cb1d154
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-23 09:11:31 -0500

    started working on pokemon lab

---
+++ IntermediateWeb/Lab2/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const pokemon = require("pokemon")
+
+app.get("/", (req, res) => {
+    res.send("I am the Pokemon root route.");
+})
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon.random())
+})
+
+app.get("/dex", (req,res) => {
+    let pokemonArr = []
+    for (let i = 0; i < 5; i++) {
+        pokemonArr.push(pokemon.random())
+    }
+    res.send(`My Pokedex: ${pokemonArr}`)
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
