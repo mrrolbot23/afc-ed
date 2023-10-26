c673b8189abd8ac31edf262ebeb0bc720601bcf5
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-10-26 09:10:29 -0500

    completed Lab3

--- IntermediateWeb/Bitcoin/index.js
+++ IntermediateWeb/Bitcoin/index.js
@@ -3,17 +3,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// This line ensures that we have access to the public folder in our project
 app.use(express.static("public"));
 
+// These line allow us to parse through the body of our requests below
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// API Enpoint URL
 const endPointUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
+// Route redericts the user to our Home Page
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
 
+// Home Page route.
 app.get("/home", (req, res) => {
   fetch(endPointUrl)
     .then((response) => {
@@ -28,7 +33,7 @@ app.get("/home", (req, res) => {
         style: "currency",
         currency: "USD",
       }).format(data.bpi["USD"]["rate_float"]);
-      selected = req.body.currency
+      selected = req.body.currency;
       res.render("home.ejs");
     })
     .catch((err) => {
@@ -51,7 +56,7 @@ app.post("/pricedata", (req, res) => {
         style: "currency",
         currency: req.body.currency,
       }).format(data.bpi[req.body.currency]["rate_float"]);
-      selected = req.body.currency
+      selected = req.body.currency;
 
       res.render("home.ejs");
     })
@@ -61,6 +66,7 @@ app.post("/pricedata", (req, res) => {
     });
 });
 
+// Display Error Page if bad request happends
 app.get("/*", (req, res) => {
   res.render("error.ejs");
 });
