3561f41430d5209edec63cb744c91c5f52955004
Edwin Roldan <edwin.a.roldandelgado.mil@army.mil>
2023-11-15 08:34:06 -0600

    added react demo

---
+++ REACT/ReactDemo/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className={"doggy"}>My First App!</h1>
+      <h2 className={"horse"}>Second Line</h2>
+    </>
+  )
+}
+
+export default App
